cb458149100735dae8ff9aa658c0ade6aee04a99
TomVCQL <tom.vicquelin@limayrac.fr>
2024-01-12 17:11:06 +0100

    ajout du blocage du prix maximal du prix transport

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,4 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Définir la date maximale sur la date actuelle
     datePicker.max = new Date().toISOString().split('T')[0];
+
+    var numberInput = document.getElementById('prixTransport');
+
+    // Ajouter un écouteur d'événements pour intercepter les changements de valeur
+    numberInput.addEventListener('input', function () {
+        // Valider la valeur par rapport à la limite maximale (300)
+        if (parseInt(numberInput.value) > 300) {
+            // Si la valeur est supérieure à 300, ajustez-la à 300
+            numberInput.value = 300;
+        }
+    });
 });
