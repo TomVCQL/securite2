d5a3c61e857291493cc31203bd7bf0aeed2febd4
TomVCQL <tom.vicquelin@limayrac.fr>
2024-01-12 21:20:44 +0100

    ajout coordonnee bancaire

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Récupérer l'élément input de type date par son ID
+    /**********************************************************/
     if(datePicker = document.getElementById('datePicker'))
     {
         datePicker.max = new Date().toISOString().split('T')[0];
     }
     
+    
+/**************************************************************/
+    if(numberInput = document.getElementById('prixTransport'))
+    {
+        numberInput.addEventListener('input', function () {
+            if (parseInt(numberInput.value) > 300) {
+                numberInput.value = 300;
+            }
+        });
+    }
+    
+    var iban = document.getElementById('ibanId');
+    var submitIban = document.getElementById('submitIban');
 
-    var numberInput = document.getElementById('prixTransport');
-
-    // Ajouter un écouteur d'événements pour intercepter les changements de valeur
-    numberInput.addEventListener('input', function () {
-        // Valider la valeur par rapport à la limite maximale (300)
-        if (parseInt(numberInput.value) > 300) {
-            // Si la valeur est supérieure à 300, ajustez-la à 300
-            numberInput.value = 300;
-        }
-    });
+    submitIban.disabled = true;
+        iban.addEventListener('input', function() {
+            if(iban.value.length == 27) {
+                submitIban.disabled = false;
+            }
+            else{
+                submitIban.disabled = true;
+            }
+        });
 });
